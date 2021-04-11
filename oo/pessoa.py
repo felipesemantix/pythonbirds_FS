01031c57e630b01f27f190deba958d658dcce18e
Felipe Spaulonci <Darezzo7notas>
2021-04-11 17:37:52 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,11 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
